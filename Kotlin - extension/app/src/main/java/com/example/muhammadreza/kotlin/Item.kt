6fe1f1c0293bb52aba7e434352496511debc5039
muhammadreza <reza_separo29@yahoo.co.id>
2018-09-06 05:56:01 +0700

    kotlin-extension

---
+++ Kotlin - extension/app/src/main/java/com/example/muhammadreza/kotlin/Item.kt
@@ -0,0 +1,6 @@
+package com.example.muhammadreza.kotlin
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Item (val name: String?, val image: Int?): Parcelable
